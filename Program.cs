caf4a71381d4113caa9e8a2547dfc1ad02a4c31a
vitoriahelen-git <vitoriahellen3004@gmail.com>
2020-09-05 21:48:31 -0300

    Resolução de exercicio

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'm so far from the line, yeah");
+            Console.WriteLine("I'm too deep in my mind, yeah");
+            Console.WriteLine("If she calls, I'll be right there");
+            Console.WriteLine("That's three calls in a night, yeah\n");
+            Console.WriteLine("Well, tell me what you think about");
+            Console.WriteLine("Staying right here instead");
+            Console.WriteLine("I didn't hear a word she said");
+            Console.WriteLine("I wasn't that high, I swear\n");
+            Console.WriteLine("Well girl, what do you think about");
+            Console.WriteLine("Staying right here in bed");
+            Console.WriteLine("I didn't hear a word you said");
+            Console.WriteLine("I wasn't that high, I swear\n");
+            Console.WriteLine("It's happening again");
+            Console.WriteLine("Well, I don't give a fuck about your friends");
+            Console.WriteLine("I'm right here, here\n");
+            Console.WriteLine("Oh, baby take a look around");
+            Console.WriteLine("I'm the only one that hasn't walked out");
+            Console.WriteLine("I'm right here, here\n");
+            Console.WriteLine("I'm right here, here\n");
+            Console.WriteLine("One draw at a time, yeah");
+            Console.WriteLine("One more, you'll be fine, yeah");
+            Console.WriteLine("And I swear she was right there");
+            Console.WriteLine("The blue glow and the night wear\n");
+            Console.WriteLine("Tell me what you think about");
+            Console.WriteLine("Staying right here instead");
+            Console.WriteLine("I didn't hear a word she said");
+            Console.WriteLine("Wasn't that high, I swear\n");
+            Console.WriteLine("And she don't wanna fuck around");
+            Console.WriteLine("And I don't wanna leave this bed");
+            Console.WriteLine("I didn't hear a word she said");
+            Console.WriteLine("Wasn't that high, I swear\n");
+            Console.WriteLine("It's happening again");
+            Console.WriteLine("Well, I don't give a fuck about your friends");
+            Console.WriteLine("I'm right here, here\n");
+            Console.WriteLine("Oh, baby take a look around");
+            Console.WriteLine("I'm the only one that hasn't walked out");
+            Console.WriteLine("I'm right here, here\n");
+            Console.WriteLine("I'm always ready to go!");
+            Console.WriteLine("I'm calling you, pick up your phone");
+            Console.WriteLine("I'm on my own");
+            Console.WriteLine("Girl, I've been taking it slow");
+            Console.WriteLine("You know I've been taking it slow\n");
+            Console.WriteLine("You're sitting alone, why are you sitting alone?");
+            Console.WriteLine("Baby just pick up your phone, oh");
+            Console.WriteLine("'Cause I've been rolling all damn night, woah\n");
+            Console.WriteLine("It's happening again");
+            Console.WriteLine("Well, I don't give a fuck about your friends");
+            Console.WriteLine("I'm right here, here\n");
+            Console.WriteLine("Oh, baby take a look around");
+            Console.WriteLine("I'm the only one that hasn't walked out");
+            Console.WriteLine("I'm right here, here\n");
+            Console.WriteLine("Compositor: Mitchel Cave/ Christian Anthony/ Cliton Cave");
+            Console.WriteLine("Fonte: Musixmatch");
+        }
+    }
+}
